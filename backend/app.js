93d0257bfcf2de51b6e75904d05535f41cb00680
Arsya <42167020+arsyaprasamya@users.noreply.github.com>
2021-10-22 08:16:35 +0700

    Update some codes

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ app.get('/', function (req, res) {
 app.get('/flickr', function (req, res) {
   const { tags } = req.query
   request(
-    `https://www.flickr.com/services/feeds/photos_public.gne?method=flickr.photos.search&api_key=51ea18be5baef1e0a9887a986b3bd958&safe_search=true&format=json&nojsoncallback=1&tags=${tags}`,
+    `https://www.flickr.com/services/feeds/photos_public.gne?method=flickr.photos.search&api_key=e6cc26d3a165607bfaee1070476ee8ce&safe_search=true&format=json&nojsoncallback=1&tags=${tags}`,
     function (error, response, body) {
       console.error('error:', error)
       console.log('statusCode:', response && response.statusCode)
